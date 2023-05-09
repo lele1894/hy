a29ebaa1663370d373985356057ba67e5a0e7d4e
lele1894 <lele1894@163.com>
2023-05-09 23:40:38 +0800

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -42,7 +42,7 @@ echo "$config" > config.json
 # 输出结果
 echo "config.json 服务端配置写入"
 
-ipv4=$(curl -s https://api.ipify.org)
+ipv4=$(curl -s ipv4.ip.sb)
 echo "本机外网IPv4为：$ipv4"
 
 touch ipv4.json
@@ -79,6 +79,43 @@ echo "$ip" > ipv4.json
 # 输出结果
 echo "ipv4客户端配置写入"
 
-echo "运行请用以下代码"
+ipv6=$(curl -s ipv6.ip.sb)
+echo "本机外网IPv6为：$ipv6"
+
+touch ipv6.json
+ip6='
+{
+"server": "['$ipv6']:9669",
+"protocol": "udp",
+"up_mbps": 20,
+"down_mbps": 100,
+"alpn": "h3",
+"acl": "acl/routes.acl",
+"mmdb": "acl/Country.mmdb",
+"http": {
+"listen": "127.0.0.1:10809",
+"timeout" : 300,
+"disable_udp": false
+},
+"socks5": {
+"listen": "127.0.0.1:10808",
+"timeout": 300,
+"disable_udp": false
+},
+"auth_str": "28fc7a",
+"server_name": "www.bing.com",
+"insecure": true,
+"retry": 3,
+"retry_interval": 3,
+"fast_open": true,
+"hop_interval": 60
+}
+'
+# 将内容写入文件
+echo "$ip6" > ipv6.json
+# 输出结果
+echo "ipv6客户端配置写入"
+
+echo "客户端文件在/root/hy/文件夹,运行请用以下代码"
 echo "cd hy"
 echo "./hysteria -c config.json server"
